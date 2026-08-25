b24947fb176128f25e248897a2de13bfcb02bfb3
Victoria Shtern <74970120+victoriashtern@users.noreply.github.com>
2026-08-24 23:59:42 -0400

    Update countries list with new operations
    Added and modified list operations for countries.

--- List/Example/basic.py
+++ List/Example/basic.py
@@ -1,1 +1,23 @@
 
+countries =["brazil",
+            "Canada",
+            "denmark",
+        ]
+
+countries.append("irland")
+#insert element at position 0
+countries.insert(0,"Afganistan")
+countries.remove("Canada")
+#print(countries)
+#countries.pop(1)
+#access elemnt
+print(countries[1]) #first elemet of the list
+
+
+#print("Removed country "+ countries.pop() )
+#print("Acces to the element in the list " + countries[0])
+#print(countries)
+
+#countries =["brazil","Canada","denmark"]
+#countries.pop(-10)
+#print(countries)
