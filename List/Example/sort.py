54ff5a8f18af03d01dac5a80414506f1f355f329
Victoria Shtern <74970120+victoriashtern@users.noreply.github.com>
2026-08-25 00:10:59 -0400

    Add sorting functionality to countries list

---
+++ List/Example/sort.py
@@ -0,0 +1,11 @@
+countries =["brazil","canada","denmark"]
+countries.append("irland")
+
+countries.insert(0,"afganistan")
+#print(countries)
+#sort the list
+countries.sort()
+print(countries.sort())
+print(countries)
+#countries.sort(reverse=True)
+#print(countries)
