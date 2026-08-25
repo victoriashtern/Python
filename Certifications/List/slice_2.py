1efcb55531e38509d2ffb1bdef98f7331d0a5991
Victoria Shtern <74970120+victoriashtern@users.noreply.github.com>
2026-08-25 00:27:51 -0400

    Delete Certifications/List/slice_2.py

--- Certifications/List/slice_2.py
+++
@@ -1,5 +0,0 @@
-list =[0,1,2,3,4,5,6,7,8,9]
-#print(list[1:7:2]) 
-#print(list[::2]) #returns on even positions
-#print(list[1::2])  # returns on odd positions
-print(list[::-1])  # returns in reverse order
