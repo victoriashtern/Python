943e6fb1568ee3395cc43906cb619bb0239a839e
Victoria Shtern <74970120+victoriashtern@users.noreply.github.com>
2026-08-25 00:06:53 -0400

    Create multi_dimention.py with list examples
    Added a multi-dimensional list example with countries and a matrix.

---
+++ List/Example/multi_dimention.py
@@ -0,0 +1,10 @@
+countries =['Argentina','Belice','Brazil','Canada','Danmark','Estonia','Zambia']
+list =[0,1,2,3,4,5,6,7,8,9]
+matrix =[[0,1],[2,3],[4,5],[],"d"]
+x=matrix[1],[0]
+
+print(matrix)
+#print(list[len(list)])
+print(list.remove(33))
+#print(list.pop(3))
+#print(list)
