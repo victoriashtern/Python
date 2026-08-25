401bb99a5f768c5e084f0bc2cdae5502e593edc8
Victoria Shtern <74970120+victoriashtern@users.noreply.github.com>
2026-08-25 00:08:27 -0400

    Enhance slice.py with list slicing examples
    Added examples of list slicing in Python.

--- List/Example/slice.py
+++ List/Example/slice.py
@@ -7,3 +7,9 @@ list1=list[1:5] #[1, 2, 3, 4]
 
 name ="banana"
 print(name[2:4]) 
+
+list =[0,1,2,3,4,5,6,7,8,9]
+#print(list[1:7:2]) 
+#print(list[::2]) #returns on even positions
+#print(list[1::2])  # returns on odd positions
+print(list[::-1])  # returns in reverse order
